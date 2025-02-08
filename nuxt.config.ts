841c362684b6d235240fe13d405775b9e6fdc14c
Ethosa <social.ethosa@gmail.com>
2025-02-08 20:50:25 +0700

    feat: connect yandex metrika

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/tailwindcss',
+    [
+      'yandex-metrika-module-nuxt3',
+      {
+        id: '99845495',
+        webvisor: true,
+      }
+    ]
   ],
   tailwindcss: {
     exposeConfig: true,
