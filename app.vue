6f062ac54e9c689ae8de01f495b1ed3ce2c6f4e6
Ethosa <social.ethosa@gmail.com>
2025-02-09 18:53:52 +0700

    chore: add goggle site verification

--- app.vue
+++ app.vue
@@ -25,6 +25,9 @@ useHead({
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "" },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200..800;1,200..800&display=swap' },
+  ],
+  meta: [
+    { name: 'google-site-verification', content: 'google-site-verification=dmrePOvPNCn7YNqHGEZoBkUUFYVS20tCT1kjkBrHck0' },
   ]
 })
 
