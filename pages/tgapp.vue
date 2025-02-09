cb80ec16d95ae5e14a17537c7d52f8bb215acbcc
Ethosa <social.ethosa@gmail.com>
2025-02-10 00:13:33 +0700

    feat: improve tg webapp

--- pages/tgapp.vue
+++ pages/tgapp.vue
@@ -1,9 +1,44 @@
 <template>
-  <div class="">
-    TG APP
+  <div
+    class="w-screen h-screen flex flex-col items-center justify-center"
+  >
+    <div
+      v-show="tg && tg.initDataUnsafe.user"
+    >
+      Welcome, {{ tg.initDataUnsafe.user.username }}!
+    </div>
+    <div
+      v-show="!tg || !tg.initDataUnsafe.user"
+    >
+      Oops! Something went wrong!
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
+
+<script setup>
+
+definePageMeta({
+  title: '$WHY'
+})
+
+
+useHead({
+  script: [
+    { src: 'https://telegram.org/js/telegram-web-app.js?56' }
+  ]
+})
+
+
+const isLoading = ref(false);
+
+const tg = undefined;
+
+
+onMounted(() => {
+  if (window.Telegram.WebApp) {
+    tg = window.Telegram.WebApp;
+  }
+})
 
 </script>
