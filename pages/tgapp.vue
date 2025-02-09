6b2bfb6f3061c88df439f8c1c2f1cef7076db237
Ethosa <social.ethosa@gmail.com>
2025-02-10 00:16:28 +0700

    feat: improve tg webapp

--- pages/tgapp.vue
+++ pages/tgapp.vue
@@ -3,12 +3,12 @@
     class="w-screen h-screen flex flex-col items-center justify-center"
   >
     <div
-      v-show="tg && tg.initDataUnsafe.user"
+      v-if="tg && tg.initDataUnsafe.user"
     >
       Welcome, {{ tg.initDataUnsafe.user.username }}!
     </div>
     <div
-      v-show="!tg || !tg.initDataUnsafe.user"
+      v-else-if="!tg || !tg.initDataUnsafe.user"
     >
       Oops! Something went wrong!
     </div>
@@ -20,24 +20,25 @@
 
 definePageMeta({
   title: '$WHY'
-})
+});
 
 
 useHead({
   script: [
     { src: 'https://telegram.org/js/telegram-web-app.js?56' }
   ]
-})
+});
 
 
 const isLoading = ref(false);
-
-const tg = undefined;
+const tg = ref(undefined);
 
 
 onMounted(() => {
   if (window.Telegram.WebApp) {
-    tg = window.Telegram.WebApp;
+    tg.value = window.Telegram.WebApp;
+    tg.value.setBottomBarColor('#04060C');
+    tg.value.setHeaderColor('#04060C');
   }
 })
 
