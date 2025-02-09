44758b3157d9691ae66b1c787aa4c12b205b0d82
Ethosa <social.ethosa@gmail.com>
2025-02-09 16:42:06 +0700

    refactor: optimize performance

--- app.vue
+++ app.vue
@@ -21,7 +21,7 @@ useHead({
     { rel: 'icon', type: 'image/x-jpg', href: '/icon.jpg' },
     { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
     { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: "" },
-    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swa' },
+    { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200..800;1,200..800&display=swap' },
   ]
 })
 
@@ -44,6 +44,7 @@ useSeoMeta({
   font-optical-sizing: auto;
   font-weight: 400;
   font-style: normal;
+  font-display: swap;
 }
 
 * {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const year = 60 * 60 * 24 * 365;
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
@@ -18,5 +21,19 @@ export default defineNuxtConfig({
     viewer: true,
     // and more...
   },
-  ssr: true
+  ssr: true,
+  nitro: {
+    routeRules: {
+      "/img/**": {
+        headers: {
+          'cache-control': `public,max-age=${year},s-maxage=${year}`,
+        }
+      },
+      "/_nuxt/**": {
+        headers: {
+          'cache-control': `public,max-age=${year},s-maxage=${year}`,
+        }
+      },
+    }
+  }
 })
